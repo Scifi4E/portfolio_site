813baee2b4061a11547d662639edf0794f73d5bb
Scifi4E <54595994+Scifi4E@users.noreply.github.com>
2020-06-05 16:08:08 -0400

    Update ghostwriter.js

--- js/ghostwriter.js
+++ js/ghostwriter.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
-    var dataText = [ "Changing,","Evolving,","Learning,", "Experimenting,",  "Developing." ];
+    var dataText = [ "Changing","Learning", "Experimenting",  "Developing" "Evolving"];
     
     // type one text in the typwriter
     // keeps calling itself until the text is finished
